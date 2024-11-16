95f5bb618582bd93c456a0616d9bd2e2e1d23b92
nicosj <nico@gmail.com>
2024-11-16 09:58:51 -0300

    fix: app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "frgp.utn.edu.com"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "frgp.utn.edu.com"
         minSdk = 30
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
 

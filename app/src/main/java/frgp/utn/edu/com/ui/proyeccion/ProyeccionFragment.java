f8a717bd21edb9c3508b15ebe28424c4c6733d0e
nicosj <nico@gmail.com>
2024-11-19 11:41:24 -0300

    fix: proyeccion

--- app/src/main/java/frgp/utn/edu/com/ui/proyeccion/ProyeccionFragment.java
+++ app/src/main/java/frgp/utn/edu/com/ui/proyeccion/ProyeccionFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -15,7 +18,28 @@ public class ProyeccionFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_proyeccion,container,false);
         ((AppCompatActivity)getActivity()).setSupportActionBar(view.findViewById(R.id.toolbar));
-        //initViews(view);
+        initViews(view);
         return view;
+    }
+
+    private void initViews(View view) {
+        EditText TxtHorasUso = view.findViewById(R.id.etHorasUso);
+        EditText txtPotencia = view.findViewById(R.id.etPotencia);
+        EditText txtCosto = view.findViewById(R.id.etDiasUso);
+        TextView txtResultado = view.findViewById(R.id.Resultadoc);
+
+        Button btnCalcular = view.findViewById(R.id.btnCalcular);
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double horasUso = Double.parseDouble(TxtHorasUso.getText().toString());
+                double potencia = Double.parseDouble(txtPotencia.getText().toString());
+                double costo = Double.parseDouble(txtCosto.getText().toString());
+
+                double resultado = horasUso * potencia * costo;
+                txtResultado.setText("El costo de uso es: $" + resultado);
+            }
+        });
+
     }
 }
